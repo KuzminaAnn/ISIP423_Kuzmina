143d901d7fffb2407193ba2a2b2dfc7fd1eb983d
234945 <anna905k@icloud.com>
2025-10-14 14:50:46 +0300

    Написали абстракцию врагов

--- ISIP423_Kuzmina/Program.cs
+++ ISIP423_Kuzmina/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace TextRPG
 {
@@ -44,5 +45,39 @@ namespace TextRPG
         public override string ToString()
         {
             return $"{Name} (Восстанавливает всё здоровье)";
+        }
+    }
+    public abstract class Enemy
+    {
+        public string Name { get; protected set; }
+        public int MaxHP { get; protected set; }
+        public int CurrentHP { get; protected set; }
+        public int Attack { get; protected set; }
+        public int Defense { get; protected set; }
+        public bool IsFrozen { get; set; }
+
+        protected Random random;
+
+        protected Enemy(string name, int hp, int attack, int defense)
+        {
+            Name = name;
+            MaxHP = hp;
+            CurrentHP = hp;
+            Attack = attack;
+            Defense = defense;
+            random = new Random();
+            IsFrozen = false;
+        }
+        public virtual void TakeDamage(int damage)
+        {
+            CurrentHP -= damage;
+            if (CurrentHP < 0) CurrentHP = 0;
+        }
+        public abstract int CalculateDamage(Player player);
+        public abstract void ApplySpecialEffect(Player player);
+        public bool IsAlive => CurrentHP > 0;
+        public virtual string GetStatus()
+        {
+            return $"{Name} - HP: {CurrentHP}/{MaxHP}, Атака: {Attack}, Защита: {Defense}";
         }
     }
