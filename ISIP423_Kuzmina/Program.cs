c83a0e04ad0d72abf47465f484a7861b80b16300
234945 <anna905k@icloud.com>
2025-10-14 14:44:15 +0300

    Прописали класс, оружие и доспехи

--- ISIP423_Kuzmina/Program.cs
+++ ISIP423_Kuzmina/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextRPG
+{
+    // Базовый класс для предметов
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        protected Item(string name)
+        {
+            Name = name;
+        }
+    }
+
+    // Оружие
+    public class Weapon : Item
+    {
+        public int Attack { get; private set; }
+        public Weapon(string name, int attack) : base(name)
+        {
+            Attack = attack;
+        }
+        public override string ToString()
+        {
+            return $"{Name} (Атака: {Attack})";
+        }
+    }
+
+    // Доспехи
+    public class Armor : Item
+    {
+        public int Defense { get; private set; }
+
+        public Armor(string name, int defense) : base(name)
+        {
+            Defense = defense;
+        }
+        public override string ToString()
+        {
+            return $"{Name} (Защита: {Defense})";
+        }
+    }
