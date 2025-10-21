1f40a40555976ddcad7d6a6625b2154d2acaba2a
234983 <anna905k@icloud.com>
2025-10-21 15:12:08 +0300

    Написали приветствие

---
+++ 3ISIP423_KUZMINA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP423_KUZMINA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ДОБРО ПОЖАЛОВАТЬ В ИГРУ!");
+            Console.WriteLine("Мой автосервис(почини авто(мы не определились с названием))");
+        }
+    }
+}
