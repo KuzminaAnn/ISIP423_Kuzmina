41118a9a6691d06727444b8461f04e45219ad1d4
234983 <anna905k@icloud.com>
2025-10-21 15:46:20 +0300

    Закончили подключение бд и начали писать вступление

---
+++ 3ISIP423_KUZMINA/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP423_KUZMINA
+{
+    internal class Core
+    {
+        public static gameAAZKEntities Context = new gameAAZKEntities();
+    }
+}
--- 3ISIP423_KUZMINA/Program.cs
+++ 3ISIP423_KUZMINA/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,29 @@ namespace _3ISIP423_KUZMINA
     {
         static void Main(string[] args)
         {
+            
+            int money = 1000;
+
             Console.WriteLine("ДОБРО ПОЖАЛОВАТЬ В ИГРУ!");
             Console.WriteLine("Мой автосервис(почини авто(мы не определились с названием))");
+            Console.WriteLine($"В начале игры у вас {money} монет");
+
+            Console.WriteLine("Нажмите (1) для начала игры");
+            Console.WriteLine("Нажмите (2) для просмотря склада");
+
+            string a = Console.ReadLine();
+
+            switch (a) 
+            {
+                case "1":
+
+                    break;
+                case "2":
+                    List<Box> box = Core.Context.Box.ToList();
+                    break;
+            }
+
+
         }
     }
 }
