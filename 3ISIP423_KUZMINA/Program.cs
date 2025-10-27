4ac2264929bc7a86e9da7da287a0809e75e3bd95
anna9 <anna905k@icloud.com>
2025-10-27 11:45:37 +0300

    Прописали ремонт и штраф

--- 3ISIP423_KUZMINA/Program.cs
+++ 3ISIP423_KUZMINA/Program.cs
@@ -10,45 +10,76 @@ namespace _3ISIP423_KUZMINA
     internal class Program
     {
         public static Random random = new Random();
+        public static Customers ranCu;
         static void Main(string[] args)
         {
             
-            int money = 1000;
+            
+                int money = 1000;
 
-            Console.WriteLine("ДОБРО ПОЖАЛОВАТЬ В ИГРУ!");
-            Console.WriteLine("Мой автосервис(почини авто(мы не определились с названием))");
-            Console.WriteLine($"В начале игры у вас {money} монет");
-
-            Console.WriteLine("Нажмите (1) для начала игры");
-            Console.WriteLine("Нажмите (2) для просмотря склада");
-
-            string a = Console.ReadLine();
-
-            switch (a)
+ 
+                Console.WriteLine("ДОБРО ПОЖАЛОВАТЬ В ИГРУ!");
+                Console.WriteLine("Мой автосервис(почини авто(мы не определились с названием))");
+                Console.WriteLine($"В начале игры у вас {money} монет");
+                
+            
+            while (true)
             {
-                case "1":
-                    List<Customers> cust = Core.Context.Customers.ToList();
-                    Customers randomCust = cust[random.Next(0, cust.Count - 1)];
-                    //List<Problems> pro = Core.Context.Problems.ToList();
-                    Console.WriteLine("Игра начинается!");
-                    Console.WriteLine("К вам приехал клиент!");
-                    Console.WriteLine($"Имя: {randomCust.Name} на {randomCust.Car_brand}");
-                    Console.WriteLine($"Проблема: {randomCust.Problems.Name}");
-                    Console.WriteLine($"Деталь: {randomCust.Problems.Details.Name}");
-                    Console.WriteLine($"Цена: {randomCust.Problems.Price} монет");
-                    Console.WriteLine("Принять клиента? д/н");
-                    break;
-                case "2":
-                    List<Box> box = Core.Context.Box.ToList();
-                    //box[random.Next(0, box.Count - 1)].Details;
-                    foreach (var boxx in box)
-                    {
-                        Console.WriteLine($"{boxx.Details.Name} - {boxx.Count}"); 
-                    }
-                    break;
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Нажмите (1) для начала игры");
+                Console.WriteLine("Нажмите (2) для просмотря склада");
+
+                string a = Console.ReadLine();
+                switch (a)
+                {
+                    case "1":
+                        List<Customers> cust = Core.Context.Customers.ToList();
+                        List<Box> boxs = Core.Context.Box.ToList();
+                        ranCu = cust[random.Next(0, cust.Count - 1)];
+                        //List<Problems> pro = Core.Context.Problems.ToList();
+                        Console.WriteLine("Игра начинается!");
+                        Console.WriteLine("К вам приехал клиент!");
+                        Console.WriteLine($"Имя: {ranCu.Name} на {ranCu.Car_brand}");
+                        Console.WriteLine($"Проблема: {ranCu.Problems.Name}");
+                        Console.WriteLine($"Деталь: {ranCu.Problems.Details.Name}");
+                        Console.WriteLine($"Цена: {ranCu.Problems.Price} монет");
+                        Console.WriteLine("Принять клиента? д/н");
+
+                        if (Console.ReadLine().ToLower() == "д")
+                        {
+                            Box editCount = Core.Context.Box.First(b => b.Details.Name == ranCu.Problems.Details.Name); // находим пользователя для изменений
+                            if (editCount.Count > 0)
+                            {
+                                Console.WriteLine("Деталь заменена!");
+                                editCount.Count = editCount.Count - 1;
+                                Core.Context.SaveChanges();
+                                Console.WriteLine($"Итог: {money + ranCu.Problems.Price} монет, {ranCu.Problems.Details.Name} - {editCount.Count}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Детали нет на складе!");
+                                Console.WriteLine($"Штраф: {money - ranCu.Problems.Price} монет");
+                            }
+
+
+                        }
+                        //else
+                        //    Console.WriteLine("Вы отказали клиенту в ремонте.Ваш штраф составил { blu - bla}.");
+
+                        break;
+                    case "2":
+                        List<Box> box = Core.Context.Box.ToList();
+                        //box[random.Next(0, box.Count - 1)].Details;
+                        foreach (var boxx in box)
+                        {
+                            Console.WriteLine($"{boxx.Details.Name} - {boxx.Count}");
+                        }
+                        break;
+
+                }
+                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                Console.ReadKey();
             }
-
-
         }
     }
 }
