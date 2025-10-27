5e7372bb8ececad0bb094106d10445650eca9179
anna9 <anna905k@icloud.com>
2025-10-27 10:01:11 +0300

    Написали вывод инвентаря  и начало игры

--- 3ISIP423_KUZMINA/Program.cs
+++ 3ISIP423_KUZMINA/Program.cs
@@ -9,6 +9,7 @@ namespace _3ISIP423_KUZMINA
 {
     internal class Program
     {
+        public static Random random = new Random();
         static void Main(string[] args)
         {
             
@@ -23,19 +24,26 @@ namespace _3ISIP423_KUZMINA
 
             string a = Console.ReadLine();
 
-            switch (a) 
+            switch (a)
             {
                 case "1":
-
+                    List<Customers> cust = Core.Context.Customers.ToList();
+                    Customers randomCust = cust[random.Next(0, cust.Count - 1)];
+                    //List<Problems> pro = Core.Context.Problems.ToList();
+                    Console.WriteLine("Игра начинается!");
+                    Console.WriteLine("К вам приехал клиент!");
+                    Console.WriteLine($"Имя: {randomCust.Name} на {randomCust.Car_brand}");
+                    Console.WriteLine($"Проблема: {randomCust.Problems.Name}");
+                    Console.WriteLine($"Деталь: {randomCust.Problems.Details.Name}");
+                    Console.WriteLine($"Цена: {randomCust.Problems.Price} монет");
+                    Console.WriteLine("Принять клиента? д/н");
                     break;
                 case "2":
                     List<Box> box = Core.Context.Box.ToList();
+                    //box[random.Next(0, box.Count - 1)].Details;
                     foreach (var boxx in box)
                     {
-                        Console.WriteLine("-----------------------------");
-                        Console.WriteLine(boxx.Details.Name);
-                        Console.WriteLine(boxx.Count);
-                        Console.WriteLine("-----------------------------");
+                        Console.WriteLine($"{boxx.Details.Name} - {boxx.Count}"); 
                     }
                     break;
             }
